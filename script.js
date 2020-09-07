cf04b37aadc967212529c555e611f37bcaf89d98
therokibul <therokibul@gmail.com>
2020-09-07 20:34:23 +0600

    Loader

--- script.js
+++ script.js
@@ -3,10 +3,25 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
+//Show Loader
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide Loadeing
+function complete() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
 // Get Qoute From API
 
 async function getQuote() {
+  loading();
   const proxyUrl = "https://whispering-tor-04671.herokuapp.com/";
   const apiUrl =
     "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
@@ -26,6 +41,8 @@ async function getQuote() {
       quoteText.classList.remove("long-quote");
     }
     quoteText.innerText = data.quoteText;
+    // stop Loading
+    complete();
   } catch (error) {
     getQuote();
   }
